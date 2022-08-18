321d307b1b0316a567f6c7c3a369201753371824
Fontenot Family <fontenotfamily@Fontenots-MacBook-Pro.local>
2022-08-18 13:08:25 -0400

    I am programatically adding quotes.

---
+++ Add-Quotes.py
@@ -0,0 +1,7 @@
+
+#Add Quotes
+f = open("quotes.txt", "a")
+f.write("This is a new quote \n")
+f.write("I am learning to code, badly \n")
+f.write("I am a total plebian learning python \n")
+f.close()
